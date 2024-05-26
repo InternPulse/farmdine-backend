d2856e0716fe485a90416e21a17d47890ff505a5
Fiona26 <fionamuthoni18@gmail.com>
2024-05-26 11:00:33 +0300

    Remove cached files and added documentation

---
+++ accounts/serializers.py
@@ -0,0 +1,79 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
+from rest_framework.exceptions import ValidationError
+from .models import CustomUser, VendorProfile, RestaurantProfile
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+    )
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'phone_number')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        # Update each field in the instance with the corresponding value in validated_data
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+
+        # Update password separately to ensure proper hashing
+        password = validated_data.get('password')
+        if password:
+            instance.set_password(password)
+
+        # Save the instance to update the user record
+        instance.save()
+        return instance
+
+
+
+class VendorProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VendorProfile
+        fields = ['vendor', 'business_name', 'vendor_address']
+
+
+
+class RestaurantProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RestaurantProfile
+        fields = ['restaurant', 'business_name', 'restaurant_address']
+
+
+    
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+    
+    def save(self, **kwargs):
+        try:
+            RefreshToken(self.token).blacklist()
+        except TokenError:
+            raise ValidationError('Token is invalid or expired!')
+
+
+
+
+
+
+
