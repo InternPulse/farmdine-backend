4167c6bb64a81fb863cfe8c70c36355abbc0a99b
Fiona26 <fionamuthoni18@gmail.com>
2024-05-26 11:00:08 +0300

    Remove cached files and added documentation

--- vendor_verification/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import VendorVerification
-
-
-class VendorVerificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = VendorVerification
-        fields = ['user', 'is_verified', 'request_date', 'verification_date']
--- vendor_verification/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import request_verification, get_verification_status
-
-urlpatterns = [
-    path('api/verification',
-         request_verification, name='request_verification'),
-    path('api/verification/<uuid:user_id>',
-         get_verification_status, name='get_verification_status'),
-]
