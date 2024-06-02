e0378961a15768cf3e6bdc7a0b80dd7283d26e3b
Collins Isiwu <collinscisiwu@outlook.com>
2024-06-02 17:23:48 +0100

    updated database config

--- farmdine/settings.py
+++ farmdine/settings.py
@@ -78,24 +78,22 @@ WSGI_APPLICATION = 'farmdine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if DEBUG is True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DATABASE_NAME'),
+        "USER": config('DATABASE_USER'),
+        "PASSWORD": config('DATABASE_PASSWORD'),
+        "HOST": config('DATABASE_HOST'),
+        "PORT": config('DATABASE_PORT'),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": config('DATABASE_NAME'),
-            "USER": config('DATABASE_USER'),
-            "PASSWORD": config('DATABASE_PASSWORD'),
-            "HOST": config('DATABASE_HOST'),
-            "PORT": config('DATABASE_PORT'),
-        }
-    }
+}
 
 
 # Password validation
